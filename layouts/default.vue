5ade4b6a018abfdec6a44053992b004cd3d227dd
Inaopeee <inaopeee@gmail.com>
2025-06-23 12:06:56 +0900

    add: 共通レイアウト

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <header>Nuxt Learning</header>
+    <slot />
+    <footer>&copy; Inaoka</footer>
+  </div>
+</template>
+<style>
+header {
+  padding: 10px;
+  background-color: #000;
+  font-weight: bold;
+  color: #fff;
+}
+footer {
+  padding: 10px;
+  background-color: orange;
+  text-align: center;
+}
+</style>
